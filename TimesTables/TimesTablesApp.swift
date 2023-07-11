85c279d497b951edb6121d469f75a8dfcc37ce4d
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-07-10 19:34:15 -0500

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Isaac Dyess on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
